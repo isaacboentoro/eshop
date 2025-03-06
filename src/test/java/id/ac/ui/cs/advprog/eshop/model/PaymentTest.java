32c9b79fd82401d23fd00345f0854f48453a5ce4
isaacboentoro <isaac.jesse@ui.ac.id>
2025-03-06 14:25:23 +0700

    [REFACTOR] Use enums in PaymentTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,14 +18,18 @@ public class PaymentTest{
     void setUp() {
         voucherData = new HashMap<>();
         voucherData.put("voucherCode", "ESHOP1234ABC5678");
-
     }
 
     @Test
     void testCreatePaymentVoucher() {
-        Payment payment = new Payment("a7c86c9d-f08b-4f57-ac05-9e95a9b36e3d", "by-voucher", "SUCCESS", voucherData);
+        Payment payment = new Payment(
+            "a7c86c9d-f08b-4f57-ac05-9e95a9b36e3d", 
+            PaymentMethod.BY_VOUCHER.getValue(), 
+            PaymentStatus.SUCCESS.getValue(), 
+            voucherData
+        );
         assertEquals("a7c86c9d-f08b-4f57-ac05-9e95a9b36e3d", payment.getId());
-        assertEquals("by-voucher", payment.getMethod());
+        assertEquals(PaymentMethod.BY_VOUCHER.getValue(), payment.getMethod());
         assertEquals(voucherData, payment.getPaymentData());
     }
 
@@ -32,24 +37,34 @@ public class PaymentTest{
     void testCreatePaymentSuccess() {
         Map<String, String> newData = new HashMap<>();
         newData.put("voucherCode", "ESHOP1234ABC5678");
-        Payment payment = new Payment("b2ec45d8-91c8-4b49-9de9-187f23abc123", "by-voucher", "SUCCESS", newData);
-        payment.setStatus("SUCCESS");
+        Payment payment = new Payment(
+            "b2ec45d8-91c8-4b49-9de9-187f23abc123", 
+            PaymentMethod.BY_VOUCHER.getValue(), 
+            PaymentStatus.SUCCESS.getValue(), 
+            newData
+        );
+        payment.setStatus(PaymentStatus.SUCCESS.getValue());
         assertEquals("b2ec45d8-91c8-4b49-9de9-187f23abc123", payment.getId());
         assertEquals("ESHOP1234ABC5678", voucherData.get("voucherCode"));
-        assertEquals("by-voucher", payment.getMethod());
-        assertEquals("SUCCESS", payment.getStatus());
+        assertEquals(PaymentMethod.BY_VOUCHER.getValue(), payment.getMethod());
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
     }
 
     @Test
     void testCreatePaymentFail() {
         Map<String, String> newData = new HashMap<>();
         newData.put("voucherCode", "MEOW");
-        Payment payment = new Payment("d5f79e3a-cc6b-4216-b54d-3ff082e2b139", "by-voucher", "REJECTED", newData);
-        payment.setStatus("REJECTED");
+        Payment payment = new Payment(
+            "d5f79e3a-cc6b-4216-b54d-3ff082e2b139", 
+            PaymentMethod.BY_VOUCHER.getValue(), 
+            PaymentStatus.REJECTED.getValue(), 
+            newData
+        );
+        payment.setStatus(PaymentStatus.REJECTED.getValue());
         assertEquals("d5f79e3a-cc6b-4216-b54d-3ff082e2b139", payment.getId());
         assertNotEquals("ESHOP1234ABC5678", newData.get("voucherCode"));
-        assertEquals("by-voucher", payment.getMethod());
-        assertEquals("REJECTED", payment.getStatus());
+        assertEquals(PaymentMethod.BY_VOUCHER.getValue(), payment.getMethod());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
     }
 }
 
